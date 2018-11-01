6eed3a54c88565e817a5ab855ef9073bd44703e6
indiagolph99 <savva.gorodetsky@gmail.com>
2018-11-01 15:00:46 +0300

    Added template support for alg5

--- alg5.cpp
+++ alg5.cpp
@@ -10,15 +10,17 @@
 #include <iostream>
 #include <string>
 // pair represents begin and end of visitor presence respectively
-struct pair {
-    int first;
-    int second;
+template <class T>
+class pair {
+public:
+    T first;
+    T second;
 };
 // arrange period by its beginning, if ends are equal
-void insert(struct pair *arr, int index) {
-    int t = index;
+template <typename T> void insert(pair<T> *arr, size_t index) {
+    size_t t = index;
     while (arr[t].second == arr[t - 1].second && arr[t - 1].first > arr[t].first) {
-        struct pair temp = arr[t];
+        pair<T> temp = arr[t];
         arr[t] = arr[t - 1];
         arr[t - 1] = temp;
         --t;
@@ -26,12 +28,12 @@ void insert(struct pair *arr, int index) {
 }
 
 //merge by the end of time period in ascending order
-void merge(int left, int right, int mid, struct pair* arr) {
-    int it_left = mid - left + 1;
-    int it_right = right - mid;
+template <typename T> void merge(size_t left, size_t right, size_t mid, pair<T>* arr) {
+    size_t it_left = mid - left + 1;
+    size_t it_right = right - mid;
     size_t i, j, k;
-    struct pair left_arr[it_left];
-    struct pair right_arr[it_right];
+    pair<T> left_arr[it_left];
+    pair<T> right_arr[it_right];
 
     for (i = 0; i < it_left; i++) {
         left_arr[i] = arr[left + i];
@@ -43,7 +45,7 @@ void merge(int left, int right, int mid, struct pair* arr) {
     i = 0;
     j = 0;
     k = left;
-    int t = 0;
+    size_t t = 0;
     while (i < it_left && j < it_right) {
         if (left_arr[i].second <= right_arr[j].second) {
             arr[k] = left_arr[i];
@@ -72,21 +74,21 @@ void merge(int left, int right, int mid, struct pair* arr) {
     }
 }
 
-void merge_sort(int left, int right, struct pair* arr) {
+template <typename T> void merge_sort(size_t left, size_t right, pair<T>* arr) {
     if (left < right) {
-        int mid = left + (right - left) / 2;
+        size_t mid = left + (right - left) / 2;
         merge_sort(left, mid, arr);
         merge_sort(mid + 1, right, arr);
         merge(left, right, mid, arr);
     }
 }
 
-int count_bans(struct pair* arr, int n) {
+template <typename T> size_t count_bans(pair<T>* arr, size_t n) {
     // two optimal show-times are chosen:
     // end and before end
-    int ban_two = arr[0].second;
-    int ban_one = ban_two - 1;
-    int count = 2;
+    T ban_two = arr[0].second;
+    T ban_one = ban_two - 1;
+    size_t count = 2;
     for (size_t i = 1; i < n; i++) {
         // if one period ends before next begins - they have no intersection points
         if (ban_two < arr[i].first) {
@@ -100,7 +102,7 @@ int count_bans(struct pair* arr, int n) {
             ban_two = arr[i].second;
             ++count;
         }
-  
+
         if (ban_two > arr[i].first) {
             if (ban_one < arr[i].first) {
                 ban_one = ban_two;
@@ -114,11 +116,11 @@ int count_bans(struct pair* arr, int n) {
 
 
 int main() {
-    int n = 0;
-    int count = 0;
-    struct pair* arr = nullptr;
+    size_t n = 0;
+    size_t count = 0;
+    pair<int>* arr = nullptr;
     std::cin >> n;
-    arr = new struct pair[n];
+    arr = new pair<int>[n];
     for (size_t i = 0; i < n; i++) {
         std::cin >> arr[i].first >> arr[i].second;
     }
